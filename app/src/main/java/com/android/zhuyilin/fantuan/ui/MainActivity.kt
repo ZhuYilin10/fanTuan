1bc4008a6be63b1c50d9425e30543b3b8d8c9fab
ZhuYilin10 <yeying10@hotmail.com>
2017-12-07 22:36:45 +0800

    项目使用 dataBinding

--- app/src/main/java/com/android/zhuyilin/fantuan/ui/MainActivity.kt
+++ app/src/main/java/com/android/zhuyilin/fantuan/ui/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.android.zhuyilin.fantuan.ui
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.android.zhuyilin.fantuan.R
+import com.android.zhuyilin.fantuan.databinding.ActivityMainBinding
+import com.android.zhuyilin.fantuan.model.Subject
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val subject = Subject()
+        subject.title = "99999"
+        val dataBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        dataBinding.subject = subject
     }
 }
